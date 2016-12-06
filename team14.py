9366141770c5d249bda155df0a4fb67bf02c96a2
jmacas <jmacas@kapoleihigh.org>
2016-12-06 10:30:53 -1000

    one of the methods
    only collude when trusted opponent

--- team14.py
+++ team14.py
@@ -5,7 +5,7 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
-
+import random
 team_name = 'The name the team gives to itself' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
@@ -17,16 +17,14 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if their_history[-1:] == 'c' and their_history[-2:-1] == 'c':
+        return 'c'
+    elif their_history[-1:] == 'c' and their_history[-2:-1] == 'b':
+        return 'b'
+    elif their_history[-1:] == 'b' and their_history[-2:-1] == 'c':
+        return 'b'
+    else:
+        return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
